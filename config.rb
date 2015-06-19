8a986843c50ad50a07a7b1f2e7270cb91d1dfc8b
Peter Mwangi <reds2anfield@gmail.com>
2015-06-19 12:13:08 +0300

    Initial commit

---
+++ config.rb
@@ -0,0 +1,5 @@
+require "susy"
+css_dir = 'css'
+sass_dir = 'components/sass'
+javascripts_dir = 'js'
+output_style = :nested
---
+++ gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.initConfig({
+		uglify: {
+			my_target: {
+				files: {
+					'js/script.js': ['components/js/*.js']
+				} //files
+			} //my_target
+		}, //uglify
+		compass: {
+			dev: {
+				options: {
+					config: 'config.rb'
+				} //options
+			} //dev
+		}, //compass
+		watch: {
+		  options: { livereload: true },
+		  scripts: {
+		  	files: ['components/js/*.js'],
+		    tasks: ['uglify'] 
+		  }, //script
+		  sass: {
+		  	files: ['components/**/*.scss'],
+		  	tasks: ['compass:dev']
+		  }, //sass
+		  html: {
+		  	files: ['*.html']
+		  }
+		} //watch
+	}) //initConfig
+	grunt.registerTask('default', 'watch');
+} //exports
